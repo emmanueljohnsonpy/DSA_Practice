482be95161e8e82be3be8d405f0dd497b79d2c9f
emmanueljohnsonpy <emmanueljohnsonpy@gmail.com>
2024-11-16 23:01:30 +0530

    day seven completed

---
+++ Graph/program_8.py
@@ -0,0 +1,49 @@
+
+
+# To Implement DFS Using Recursion / Unweighted Undirected Graph
+
+def add_node(v):
+    if v in graph:
+        print(v, "is already present in Graph")
+    else:
+        graph[v]=[]
+    
+def add_edge(v1, v2):
+    if v1 not in graph:
+        print(v1, "is not present in the Graph")
+    elif v2 not in graph:
+        print(v2, "is not persent in the Graph")
+    else:
+        # list1=[v2, cost]
+        # list2=[v1, cost]
+        graph[v1].append(v2)
+        graph[v2].append(v1) 
+
+def DFS(node, visited, graph):
+    if node not in graph:
+        print("Node is not present in the graph")
+        return
+    if node not in visited:
+        print(node)
+        visited.add(node)
+        for i in graph[node]:
+            DFS(i, visited, graph)  # In weighted graph instead of calling i call i[0]
+
+visited=set()
+graph={}
+add_node("A")
+add_node("B")
+add_node("C")
+add_node("D")
+add_node("E")
+
+add_edge("A", "B")
+add_edge("B", "E")
+add_edge("A", "C")
+add_edge("A", "D")
+add_edge("B", "D")
+add_edge("C", "D")
+add_edge("E", "D")
+print(graph)
+
+DFS("k", visited, graph)
---
+++ Graph/program_9.py
@@ -0,0 +1,65 @@
+
+
+# DFS Using Iterative Approach | Using Stack
+
+def add_node(v):
+    if v in graph:
+        print(v, "is already present in Graph")
+    else:
+        graph[v]=[]
+    
+def add_edge(v1, v2):
+    if v1 not in graph:
+        print(v1, "is not present in the Graph")
+    elif v2 not in graph:
+        print(v2, "is not persent in the Graph")
+    else:
+        # list1=[v2, cost]
+        # list2=[v1, cost]
+        graph[v1].append(v2)
+        graph[v2].append(v1) 
+
+""" def DFS(node, visited, graph):
+    if node not in graph:
+        print("Node is not present in the graph")
+        return
+    if node not in visited:
+        print(node)
+        visited.add(node)
+        for i in graph[node]:
+            DFS(i, visited, graph)  # In weighted graph instead of calling i call i[0]
+"""
+
+def DFSiterative(node, graph):
+    visited=set()
+    if node not in graph:
+        print("Node is not prsent in the Graph")
+        return
+    stack=[]
+    stack.append(node)
+    while stack:
+        current=stack.pop()
+        if current not in visited:
+            print(current)
+            visited.add(current)
+            for i in graph[current]:
+                stack.append(i)
+
+# visited=set()
+graph={}
+add_node("A")
+add_node("B")
+add_node("C")
+add_node("D")
+add_node("E")
+
+add_edge("A", "B")
+add_edge("B", "E")
+add_edge("A", "C")
+add_edge("A", "D")
+add_edge("B", "D")
+add_edge("C", "D")
+add_edge("E", "D")
+print(graph)
+
+DFSiterative("A", graph)

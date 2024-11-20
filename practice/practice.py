056b0a8675385d27582465749a420b00fb0544f1
emmanueljohnsonpy <emmanueljohnsonpy@gmail.com>
2024-11-20 21:57:13 +0530

    day 10 completed

--- practice/practice.py
+++ practice/practice.py
@@ -32,6 +32,60 @@ class BinarySearchTree:
                 current=current.right
             else:
                 return
+    
+    def contains(self, value):              # Iterative approach
+        current=self.root
+        while current:
+            if value==current.value:
+                return True
+            elif value<current.value:
+                current=current.left
+            else:
+                current=current.right
+        return False
+
+    def delete(self, value):
+        def find_min(node):
+            while node.left:
+                node=node.left
+            return node
+        
+        def delete_node(node, value):
+            if not node:
+                return None
+            if value<node.value:
+                node.left=delete_node(node.left, value)
+            elif value>node.value:
+                node.right=delete_node(node.right, value)
+            else:
+                if not node.left:
+                    return node.right
+                elif not node.right:
+                    return node.left
+                
+                temp=find_min(node.right)
+                node.value=temp.value
+                node.right=delete_node(node.right, temp.value)
+            return node
+        self.root=delete_node(self.root, value)
+
+    def inorder(self, node, result):
+        if node:
+            self.inorder(node.left, result)
+            result.append(node.value)
+            self.inorder(node.right, result)
+    
+    def preorder(self, node, result):
+        if node:
+            result.append(node.value)
+            self.preorder(node.left, result)
+            self.preorder(node.right, result)
+    
+    def postorder(self, node, result):
+        if node:
+            self.postorder(node.left, result)
+            self.postorder(node.right, result)
+            result.append(node.value)
 
 bst=BinarySearchTree()
 
@@ -40,3 +94,20 @@ bst.insert(20)
 bst.insert(30)
 bst.insert(40)
 bst.insert(50)
+
+print("Contains 10 :", bst.contains(10))
+print("Contains 60 :", bst.contains(60))
+
+inorder_result = []
+bst.inorder(bst.root, inorder_result)
+print("Inorder Traversal:", inorder_result)
+
+preorder_result = []
+bst.preorder(bst.root, preorder_result)
+print("Preorder Traversal:", preorder_result)
+
+postorder_result = []
+bst.postorder(bst.root, postorder_result)
+print(postorder_result)
+
+bst.delete(50)

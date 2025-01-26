93c0af705ba29c577784b2c58acf72d9054b3625
emmanueljohnsonpy <emmanueljohnsonpy@gmail.com>
2025-01-26 09:32:51 +0530

    recursion basics

---
+++ DSA_1/recursion.py
@@ -0,0 +1,28 @@
+"Factorial"
+
+""" def factorial(n):
+    if n == 0 or n == 1:
+        return 1
+    return n * factorial(n - 1)
+
+print(factorial(5)) """
+
+
+"Fibonacci"
+
+""" def fibonacii(n):
+    if n <= 1:
+        return n
+    return fibonacii(n - 1) + fibonacii(n - 2)
+
+print(fibonacii(6)) """
+
+
+"Sum of digits"
+
+def sum_of_digits(n):
+    if n == 0:
+        return 0
+    return n % 10 + sum_of_digits(n // 10)
+
+print(sum_of_digits(12345))
